bba4d5b8d470b650a1987a0a0b93d6bae244cddb
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-15 23:57:06 +0600

    Design banner component

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Button, Container, FormControl, InputGroup } from "react-bootstrap";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <section>
-      <h1>Banner</h1>
+    <section className="banner mt-3 mb-5">
+      <Container>
+        <div className="text-center">
+          <h1 className="mb-4">Best Food waiting for your belly</h1>
+          <InputGroup className="mb-3 rafat-input-field-width mx-auto">
+            <FormControl
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+              className="rounded-pill me-1"
+            />
+            <Button
+              variant="danger"
+              id="button-addon2"
+              className="rounded-pill"
+            >
+              Button
+            </Button>
+          </InputGroup>
+        </div>
+      </Container>
     </section>
   );
 };
