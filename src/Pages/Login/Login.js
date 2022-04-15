79f219353037d7227bbc6d76c19e8690de3863ae
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-16 00:15:41 +0600

    Design login component

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import logo from "../../images/logo2.png";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login Component</h1>
-    </div>
+    <Container className="banner mb-5">
+      <Row className="px-2">
+        <Col md="5" className="mx-auto">
+          <img
+            src={logo}
+            alt=""
+            srcset=""
+            className="mb-5 w-50 mx-auto d-block"
+          />
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Enter password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="danger" type="submit" className="w-100">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
