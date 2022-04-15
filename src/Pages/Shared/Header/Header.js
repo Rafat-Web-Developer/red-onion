ba2861ba53c51343bf05d70a38cb5db524de5c98
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-15 15:53:16 +0600

    Update header navbar design

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,30 +1,27 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import logo from "../../../images/logo2.png";
 
 const Header = () => {
   return (
-    <header>
-      <Navbar bg="light" expand="lg">
+    <header className="py-3">
+      <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="site_logo" style={{ width: "150px" }} />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className="fw-bold">
+                Login
+              </Nav.Link>
+              <Nav.Link
+                href="#link"
+                className="fw-bold btn btn-danger text-white rounded-pill px-3"
+              >
+                Signup
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
